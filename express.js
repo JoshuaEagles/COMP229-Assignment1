e31f5ea48a907aaa1b2971e52f579ac6632fb7dd
JoshuaEagles <joshuaeagles1@gmail.com>
2020-10-02 01:15:10 -0400

    Implement all the pages + proper nav active

--- express.js
+++ express.js
@@ -7,7 +7,6 @@ let cookieParser = require('cookie-parser');
 let logger = require('morgan');
 
 let indexRouter = require('./routes/index');
-let usersRouter = require('./routes/users');
 
 let app = express();
 
@@ -22,7 +21,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,7 +35,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', { title: "Error" } );
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
 
-/* GET home page. */
+/* GET Root page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.redirect('/home')
+});
+
+/* GET Home page. */
+router.get('/home', function(req, res, next) {
+	res.render('index', { title: 'Home', partialName: 'home' });
+});
+
+/* GET About Me page. */
+router.get('/about', function(req, res, next) {
+	res.render('index', { title: 'About Me', partialName: 'about' });
+});
+
+/* GET Projects page. */
+router.get('/projects', function(req, res, next) {
+	res.render('index', { title: 'Projects', partialName: 'projects' });
+});
+
+/* GET Services page. */
+router.get('/services', function(req, res, next) {
+	res.render('index', { title: 'Services', partialName: 'services' });
+});
+
+/* GET Contact Me page. */
+router.get('/contact', function(req, res, next) {
+	res.render('contact');
 });
 
 module.exports = router;
