44c1944beffeb123e686e0e15e42890c90155119
JoshuaEagles <joshuaeagles1@gmail.com>
2020-10-07 03:32:51 -0400

    Simplify code in contact.js

--- public/scripts/contact.js
+++ public/scripts/contact.js
@@ -1,25 +1,23 @@
 /* contact.js - Joshua Eagles - 301078033 - 2020-10-07 */
 
-window.addEventListener('load', () => 
+let submitButton = document.getElementById("submit");
+
+// Adds an event listener for the submit button that will print the form to the console and then redirect to the home page
+submitButton.addEventListener("click", (event) => 
 {
-	let submitButton = document.getElementById("submit");
+	event.preventDefault();
 
-	submitButton.addEventListener("click", (event) => 
-	{
-		event.preventDefault();
+	let firstName = document.getElementById("firstName").value;
+	let lastName = document.getElementById("lastName").value;
+	let contactNumber = document.getElementById("contactNumber").value;
+	let email = document.getElementById("email").value;
+	let message = document.getElementById("message").value;
 
-		let firstName = document.getElementById("firstName").value;
-		let lastName = document.getElementById("lastName").value;
-		let contactNumber = document.getElementById("contactNumber").value;
-		let email = document.getElementById("email").value;
-		let message = document.getElementById("message").value;
+	console.log("First Name: ", firstName);
+	console.log("Last Name: ", lastName);
+	console.log("Contact Number: ", contactNumber);
+	console.log("Email: ", email);
+	console.log("Message: ", message);
 
-		console.log("First Name: ", firstName);
-		console.log("Last Name: ", lastName);
-		console.log("Contact Number: ", contactNumber);
-		console.log("Email: ", email);
-		console.log("Message: ", message);
-
-		window.location.href="/home";
-	});
+	window.location.href="/home";
 });
